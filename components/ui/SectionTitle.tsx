822463ec52e225f96d8b7620295692754bcca0fe
Abdulrhman Mahyoub <mbmahyoub2@gmail.com>
2025-12-30 00:41:22 +0300

    fix: update SectionTitle component styles

--- components/ui/SectionTitle.tsx
+++ components/ui/SectionTitle.tsx
@@ -8,7 +8,7 @@ export default function SectionTitle({ title }: SectionTitleProps) {
   return (
     <div className="flex items-center justify-center gap-2 sm:gap-4 md:gap-6">
       <span className="h-px max-w-[200px] flex-1 bg-colorSecondaryLight opacity-50 sm:max-w-[280px] md:max-w-[320px]"></span>
-      <h2 className="text-[9px] font-semibold uppercase leading-tight tracking-[0.1em] sm:text-xs sm:tracking-[0.2em] md:text-base md:tracking-[0.3em] lg:text-lg lg:tracking-[0.35em] xl:text-xl">
+      <h2 className="text-[6px] font-semibold uppercase leading-tight tracking-[0.1em] sm:text-xs sm:tracking-[0.2em] md:text-base md:tracking-[0.3em] lg:text-base lg:tracking-[0.10em] xl:text-base">
         {title}
       </h2>
       <span className="h-px max-w-[200px] flex-1 bg-colorSecondaryLight opacity-50 sm:max-w-[280px] md:max-w-[320px]"></span>
